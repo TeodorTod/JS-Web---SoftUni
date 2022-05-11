068ab52d60c3b7146d25ed3e6f878badc77aea9c
Artos Web <teodor.at.tod@gmail.com>
2022-05-11 07:04:41 +0300

    models

---
+++ JS Back-End/10. Workshop Session and Authentication/js-back-end-cars/models/Car.js
@@ -0,0 +1,16 @@
+const { Schema, model, Types: { ObjectId } } = require('mongoose');
+
+const carSchema = new Schema({
+    name: { type: String, required: true }, 
+    description: { type: String, default: 'no_image.jpg' },
+    imageUrl: { type: String },
+    price: { type: Number, required: true, min: 0 },
+    accessories: { type: [ObjectId], default: [], ref: 'Accessory' },
+    isDeleted: { type: Boolean, default: false },
+    owner: { type: ObjectId, ref: 'User' }
+});
+
+const Car = model('Car', carSchema);
+
+
+module.exports = Car;
