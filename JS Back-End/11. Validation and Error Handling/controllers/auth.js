ccec45deb81d7afe0a17afe588bddf832f034400
Artos Web <teodor.at.tod@gmail.com>
2022-05-15 07:57:31 +0300

    controllers

---
+++ JS Back-End/11. Validation and Error Handling/controllers/auth.js
@@ -0,0 +1,38 @@
+const { validationResult } = require("express-validator");
+
+
+module.exports = {
+    registerGet(req, res) {
+        res.render('register', { title: 'Register' });
+    },
+    async registerPost(req, res) {
+        const { errors } =  validationResult(req);
+
+        try {
+            if (errors.length > 0) {
+                throw errors;
+            }
+            await req.auth.register(req.body.username, req.body.password);
+            res.redirect('/');
+        } catch (errors) {
+            console.log(errors);
+            res.render('register', { title: 'Register', errors, data: { username: req.body.username } });
+        }
+    },
+    loginGet(req, res) {
+        res.render('login', { title: 'Login' });
+    },
+    async loginPost(req, res) {
+     try {
+        await req.auth.login(req.body.username, req.body.password);
+        res.redirect('/');
+     } catch (err) {
+        console.log(err.message);
+        res.redirect('/login');
+     }
+    },
+    logout(req, res) {
+        req.auth.logout();
+        res.redirect('/');
+    }
+};
---
+++ JS Back-End/11. Validation and Error Handling/controllers/create.js
@@ -0,0 +1,24 @@
+module.exports = {
+    get(req, res) {
+        res.render('create', { title: 'Create Listing' });
+    },
+    async post(req, res) {
+        const car = {
+            name: req.body.name,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl || undefined,
+            price: Number(req.body.price),
+            owner: req.session.user.id
+        };
+        try {
+            await req.storage.createCar(car);
+            res.redirect('/');
+        } catch (errors) {
+            if (errors.name == 'ValidationError') {
+                errors = Object.values(errors.errors).map(e => ({ msg: e.message }));
+            }
+            console.log('Error creating record');
+            res.render('create', { title: 'Create Listing', errors });
+        }
+    }
+};
