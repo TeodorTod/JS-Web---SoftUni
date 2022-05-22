df55a5ad5184e356b3c59c8825d66192a451ee52
Artos Web <teodor.at.tod@gmail.com>
2022-05-22 17:39:50 +0300

    config

---
+++ JS Back-End/12. Exam Preparation/Wildlife-photograpy/config/routes.js
@@ -0,0 +1,12 @@
+const authController = require('../controllers/auth');
+const homeController = require('../controllers/home');
+const postController = require('../controllers/post');
+
+
+
+module.exports = (app) => {
+    app.use(homeController);
+    app.use(authController);
+    app.use(postController);
+
+};
