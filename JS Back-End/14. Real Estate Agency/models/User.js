b51cb7bd342ae746cc18e16a8cda6f0578a08dd5
Artos Web <teodor.at.tod@gmail.com>
2022-06-25 19:47:55 +0300

    User model

---
+++ JS Back-End/14. Real Estate Agency/models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+
+//TODO Add validation
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    hashedPassword: { type: String, required: true }
+});
+
+userSchema.index({ username: 1}, {
+    unique: true,
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
